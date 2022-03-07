9b1bef6cdd22a0834be3e2eab7697e7b082b80f2
garbaz <garbaz@t-online.de>
2022-03-07 11:26:15 +0100

    covert traits

---
+++ src/traits.rs
@@ -0,0 +1,34 @@
+use sfml::system::Vector2f;
+
+pub trait AsTupel {
+    fn as_tupel(&self) -> (f32, f32);
+}
+
+impl AsTupel for glm::Vec2 {
+    fn as_tupel(&self) -> (f32, f32) {
+        return (self.x, self.y);
+    }
+}
+
+pub trait AsGlmVector2 {
+    fn as_glm(&self) -> glm::Vec2;
+}
+
+impl<T> AsGlmVector2 for sfml::system::Vector2<T>
+where
+    T: Into<f64> + Copy,
+{
+    fn as_glm(&self) -> glm::Vec2 {
+        glm::vec2(self.x.into() as f32, self.y.into() as f32)
+    }
+}
+
+pub trait AsSfmlVector2 {
+    fn as_sfml(&self) -> sfml::system::Vector2f;
+}
+
+impl AsSfmlVector2 for glm::Vector2<f32> {
+    fn as_sfml(&self) -> sfml::system::Vector2f {
+        Vector2f::new(self.x, self.y)
+    }
+}
